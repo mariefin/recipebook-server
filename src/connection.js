d844ab321a233c8a2145ae94b1d8ff624370eb94
mariefin <maikki_maa_93@hotmail.com>
2021-04-23 19:47:17 +0300

    changed connection url.

--- src/connection.js
+++ src/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const Recipe = require("./Recipe.model");
 
-const connection = "mongodb://localhost:27017/recipe-db";
+// const connection = "mongodb://localhost:27017/recipe-db";
+const connection = "mongodb://mongo:27017/recipe-db";
 const connectDb = () => {
     return mongoose.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 }
